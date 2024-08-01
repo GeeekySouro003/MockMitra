587725e2781545432c3c76b1bd4e7d0b48688591
Souradip <dgsouro77@gmail.com>
2024-08-01 22:05:35 +0530

    Issue in dashboard route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    productionBrowserSourceMaps: true,
+  };
+  
+  export default nextConfig;
+  
