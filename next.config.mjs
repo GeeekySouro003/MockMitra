8a94c771b9917e33b0693dbb6121c199d4afd620
Souradip <dgsouro77@gmail.com>
2024-08-01 22:15:43 +0530

    Fixed the bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    productionBrowserSourceMaps: true,
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
