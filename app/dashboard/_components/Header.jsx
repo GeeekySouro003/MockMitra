0872792d07ec526fea377f1e7d736a22051482c7
Souradip <dgsouro77@gmail.com>
2024-08-06 01:06:14 +0530

    Added logo to the header

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,9 +1,17 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Header() {
   return (
     <div>
-      
+      <Image src={'/logo.svg'} width={160} height={100} alt="Logo" />
+      <ul>
+        <li>DashBoard</li>
+        <li>Questions</li>
+        <li>Upgrade</li>
+        <li>How it Work?</li>
+      </ul>
+      <UserButton />
     </div>
   )
 }
