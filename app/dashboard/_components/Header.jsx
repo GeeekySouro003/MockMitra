48beeb1782d7745ea942ae976734110415592f1f
Souradip <dgsouro77@gmail.com>
2024-08-06 01:39:48 +0530

    Updated the dashboard for small devices

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,15 +1,30 @@
+"use client"
 import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 function Header() {
+  const path =usePathname();
+  useEffect(() => {
+    console.log(path)
+  },[])
   return (
-    <div>
+    <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
       <Image src={'/logo.svg'} width={160} height={100} alt="Logo" />
-      <ul>
-        <li>DashBoard</li>
-        <li>Questions</li>
-        <li>Upgrade</li>
-        <li>How it Work?</li>
+      <ul className='hidden md:flex gap-7'>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard' && 'text-primary font-bold'}
+          `}>DashBoard</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard/questions' && 'text-primary font-bold'}
+          `}>Questions</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard/upgrade' && 'text-primary font-bold'}
+          `}>Upgrade</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+          ${path=='/dashboard/how' && 'text-primary font-bold'}
+          `}>How it Work?</li>
       </ul>
       <UserButton />
     </div>
