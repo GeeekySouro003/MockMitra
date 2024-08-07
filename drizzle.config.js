14703c8c0e89ad2f1f8a61ceab1308b0a8bc473f
Souradip <dgsouro77@gmail.com>
2024-08-08 01:18:51 +0530

    Updated the db with schema path

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,6 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./schema.ts",
+    schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
       url:'postgresql://mockmitra_owner:RM7ba8GQLlSj@ep-cool-dawn-a5ck6s83.us-east-2.aws.neon.tech/mockmitra?sslmode=require',
