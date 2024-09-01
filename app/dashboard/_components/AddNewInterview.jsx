49c3356f1e1393e6fc43ee20da4d1480766e6c16
Souradip <dgsouro77@gmail.com>
2024-09-02 02:06:07 +0530

    Implemented the add new interview dialog box

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AddNewInterview() {
+  return (
+    <div>
+      <div className='p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all'>
+        <h2 className='text-bold text-lg text-center'>+ Create New</h2>
+      </div>
+    </div>
+  )
+}
+
+export default AddNewInterview
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,6 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import AddNewInterview from './_components/AddNewInterview'
 
 function Dashboard() {
   return (
@@ -7,7 +8,11 @@ function Dashboard() {
       <div className="p-10">
 
       <h2 className='font-bold text-3xl'>DashBoard</h2>
-      <h2>Create Your Next Mockup Interview</h2>
+      <h2 className='text-gray-600'>Create Your Next Mockup Interview</h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
+        <AddNewInterview/>
+      </div>
       </div>
     </div>
   )
