d29279060a0351100ef083107ef5656ce6f64d25
Souradip <dgsouro77@gmail.com>
2024-12-13 22:01:03 +0530

    Added the question section

---
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -0,0 +1,29 @@
+import { Lightbulb } from 'lucide-react'
+import React from 'react'
+
+function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
+
+  return mockInterviewQuestion && (
+    <div className='p-5 border rounded-xl my-8'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+        {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=> (
+          <h3 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer
+            ${activeQuestionIndex==index &&"bg-blue-600 text-white"}`}>Question #{index+1}</h3>
+        ))}
+
+      </div>
+      <h2 className='my-5 text-sm md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+
+      <div className='border rounded-lg p-5 bg-blue-100 mt-15'>
+        <h2 className='flex gap-2 items-center text-blue-600'>
+          <Lightbulb/>
+          <b>Note:</b>
+        </h2>
+        <h2 className='text-sm text-primary my-2'>{process.env.NEXT_PUBLIC_QUESTION_NOTE}</h2>
+      </div>
+
+    </div>
+  )
+}
+
+export default QuestionsSection
--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -4,11 +4,14 @@ import { MockInterview } from '@/utils/schema';
 import { eq } from 'drizzle-orm';
 import { json } from 'drizzle-orm/mysql-core';
 import React, { useEffect, useState } from 'react'
+import QuestionSection from './_components/QuestionsSection';
+import QuestionsSection from './_components/QuestionsSection';
 
 function StartInterview({params}) {
 
   const[interviewData,setInterviewData]=useState();
-  const[MockInterviewQuestions,setInterviewQuestions]=useState();
+  const[mockInterviewQuestion,setInterviewQuestion]=useState();
+  const[activeQuestionIndex,setActiveQuestionIndex]=useState(0);
   useEffect(() => {
 GetInterviewDetails();
   },[]);
@@ -25,13 +28,21 @@ GetInterviewDetails();
 
     const jsonMockresp=JSON.parse(result[0].jsonMockResp);
     console.log(jsonMockresp);
-    setInterviewQuestions(jsonMockresp);
+    setInterviewQuestion(jsonMockresp);
     setInterviewData(result[0]); 
   };
 
   return (
     <div>
-      StartInterview
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        {/*Questions*/}
+        <QuestionsSection mockInterviewQuestion={mockInterviewQuestion}
+        activeQuestionIndex={activeQuestionIndex}
+        />
+
+
+        {/*Recording */}
+      </div>
     </div>
   )
 }
