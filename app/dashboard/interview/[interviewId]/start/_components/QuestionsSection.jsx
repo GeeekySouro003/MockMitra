5515647a8d2ca9054e7ef94a0d8c7567b00da2eb
Souradip <dgsouro77@gmail.com>
2025-02-05 02:11:16 +0530

    Completed the voice speech to text feature

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -1,7 +1,17 @@
-import { Lightbulb } from 'lucide-react'
+import { Lightbulb, Volume2 } from 'lucide-react'
 import React from 'react'
 
 function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
+
+  const textToSpeech=(text)=>{
+    if('speechSynthesis' in window) {
+      const speech=new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech);
+    }
+    else{
+      alert('Your browser does not support this feature!')
+    }
+  }
 
   return mockInterviewQuestion && (
     <div className='p-5 border rounded-xl my-8'>
@@ -13,6 +23,7 @@ function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
 
       </div>
       <h2 className='my-5 text-sm md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+      <Volume2 className="cursor-pointer my-5"onClick={()=>textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
 
       <div className='border rounded-lg p-5 bg-blue-100 mt-15'>
         <h2 className='flex gap-2 items-center text-blue-600'>
