f48d69e6ab935b6194e00c47896cd5928b36da74
Souradip <dgsouro77@gmail.com>
2025-02-05 02:02:51 +0530

    Added Feature:Speech to text

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,9 +1,33 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import { Mic } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useSpeechToText from 'react-hook-speech-to-text'
 import Webcam from 'react-webcam'
+//import useSpeechToText from 'react-hook-speech-to-text';
 
 function RecordAnswerSection() {
+  const[userAnswer,setUserAnswer] = useState('');
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
+  useEffect(() => {
+    results.map((result)=> {
+      setUserAnswer(prevAns=>prevAns+result?.transcript)
+    })
+
+  },[results])
+
   return (
     <div className='flex items-center justify-center flex-col'>
 <div className='flex flex-col justify-center items-center bg-black rounded-lg p-5 mt-20'>
@@ -18,7 +42,16 @@ function RecordAnswerSection() {
       mirrored={true}
       />
     </div>
-    <Button variant="outline" className="my-10">Start Recording</Button>
+    <Button variant="outline" className="my-10" 
+    onClick={isRecording?stopSpeechToText:startSpeechToText}
+    >
+      {isRecording?
+      <h2 className='text-red-600 flex gap-3'>
+        <Mic/> Stop Recording..
+      </h2>
+      : 'Record Answer'}</Button>
+      <Button onClick={()=>console.log(userAnswer)}>Show Answer</Button>
+    
     </div>
     
   )
