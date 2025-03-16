12029d01cc56f74feef88ddc2198f27154bd4256
Souradip <dgsouro77@gmail.com>
2025-03-16 18:01:41 +0530

    fetched the feedback data from db

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+"use client"
+import { db } from '@/utils/db'
+import { UserAnswer } from '@/utils/schema'
+import { eq } from 'drizzle-orm'
+import React, { useEffect, useState } from 'react'
 
-function Feedback() {
+function Feedback({params}) {
+
+const[feedbackList,setFeedbackList]=useState([]);
+  useEffect(()=>{
+     GetFeedback();
+  },[])
+  const GetFeedback=async() => {
+    const result=await db.select()
+    .from(UserAnswer)
+    .where(eq(UserAnswer.mockIdRef,params.interviewId))
+    .orderBy(UserAnswer.id);
+
+    console.log(result);
+    setFeedbackList(result);
+
+  }
   return (
     <div className='p-10'>
       <h2 className='text-2xl font-bold text-green-600'>Congratulations!</h2>
