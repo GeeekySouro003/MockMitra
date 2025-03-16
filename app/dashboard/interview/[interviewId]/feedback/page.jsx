109b6408de183114bcc7a6ff020c2206bd40182e
Souradip <dgsouro77@gmail.com>
2025-03-16 17:48:51 +0530

    Added the basic outlay in feedback page

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Feedback() {
   return (
-    <div>
-      Feedback
+    <div className='p-10'>
+      <h2 className='text-2xl font-bold text-green-600'>Congratulations!</h2>
+      <h2 className='font-bold text-xl'>Here is your Interview Feedback</h2>
+      <h2 className='text-primary text-lg my-3'>Your overall interview rating: <strong>8/10</strong></h2>
+
+      <h2 className='text-sm text-gray-600'>Find below Interview Question with Correct Answer , Your answer and feedback for improvement</h2>
     </div>
   )
 }
