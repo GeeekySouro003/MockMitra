766409801aa3c31c2f5b5c25464ccd26d427c5cb
Souradip <dgsouro77@gmail.com>
2025-04-07 21:40:45 +0530

    Completed the feedback part.Upgrade section is pending

--- app/dashboard/_components/InterviewItemCard.jsx
+++ app/dashboard/_components/InterviewItemCard.jsx
@@ -17,7 +17,7 @@ function InterviewItemCard({interview}) {
     <div className='border shadow-sm rounded-lg p-3'>
       <h2 className='font-bold text-primary'>{interview?.jobPosition}</h2>
       <h2 className='text-sm text-gray-600'>{interview?.jobExperience} Years of Experience</h2>
-      <h2 className='text-xs text-gray-400'> Created At:{interview.createdAt}</h2>
+      <h2 className='text-xs text-gray-400'> Created At: {interview.createdAt}</h2>
       <div className='flex justify-between mt-4 gap-7' >
         <Button size="sm" variant="outline" className='w-full' 
         onClick={onFeedbackPress}
--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -33,8 +33,17 @@ const router=useRouter();
   }
   return (
     <div className='p-10'>
-      <h2 className='text-2xl font-bold text-green-600'>Congratulations!</h2>
-      <h2 className='font-bold text-xl'>Here is your Interview Feedback</h2>
+      
+
+      {feedbackList?.length==0?
+    <h2 className='font-bold text-xl text-gray-700'>No Interview Feedback Record Found</h2>  
+
+    :<>
+    <h2 className='text-2xl font-bold text-green-600'>Congratulations!</h2>
+    <h2 className='font-bold text-xl'>Here is your Interview Feedback</h2>
+    
+
+      
       <h2 className='text-primary text-lg my-3'>Your overall interview rating: <strong>8/10</strong></h2>
 
     
@@ -55,7 +64,7 @@ const router=useRouter();
     </CollapsibleContent>
   </Collapsible>
 ))}
-
+</>}
 <Button onClick={()=>router.replace('/dashboard')}>Go Home</Button>
     </div>
     
